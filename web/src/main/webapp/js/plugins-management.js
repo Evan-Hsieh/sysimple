e6de43e5b93adc2631eed0a67f9c9ea3af5d80f0
Evan <dreamcoding@outlook.com>
2017-03-25 00:35:25 +0800

    finish show plugins list in web

--- web/src/main/webapp/js/plugins-management.js
+++ web/src/main/webapp/js/plugins-management.js
@@ -1,25 +1,60 @@
-$(function(){
-	alert("bangding");
-	$("#center-main-content").load("htmls/component-plugin-list-table.html");
+$(function(){	
+	$("#sidebar-menu-check-plugins").click(function(){		
+		//load the html of pulugins list table
+		$("#center-main-content").empty();
+		syncAjaxInsertRow("#center-main-content","htmls/component-plugin-list-table.html");		
+		ajaxCheckPlugins();
+	});	
+	
+	$("#sidebar-menu-check-cluster").click(function(){
+		alert($("#center-main-content").html());
+	});	
 });
 
-function showPlugins(){
 
+
+function ajaxCheckPlugins(){
 	$.ajax({
 		  type:'POST',
-		  url:'plugins-management/show-plugins',
+		  url:'plugins-management/check-plugins',
+		  dataType:'json',
 		  success: function(data){
-			  alert(data);
+			  scanJsonData(data);
+		  },
+		  error:function(){
+			  alert("error");
 		  }
 	});
-	
-
-	
-	$("#center-main-content").click(function(){
-		alert("123");
-	});
-	
-
 
 };
+
+function scanJsonData(data){
+	  for(var i=0;i<data.length;i++){
+		  //alert(data[i].name);
+		  insertRow(data[i]);
+	  }
+};
+
+function insertRow(obj){
+	syncAjaxInsertRow("#plugins-list-table","htmls/component-plugin-list-table-row.html");
+	$(".box-primary:last .col-intro div:eq(0)").append(obj.name);
+	$(".box-primary:last .col-intro div:eq(1)").append(obj.intro);
+	$(".box-primary:last #plugin-detail-row div:first").append(obj.detail);
+};
+
+//correct function!!!
+function syncAjaxInsertRow(domObj,url){
+	$.ajax({
+		  type:'POST',
+		  url:url,
+		  async:false, 
+		  dataType:'html',
+		  success: function(data){
+			  $(domObj).append(data);
+		  },
+		  error:function(){
+			  alert("error");
+		  }
+	});
+}
 
