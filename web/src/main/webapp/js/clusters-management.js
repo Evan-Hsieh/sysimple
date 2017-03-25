9c61eb9b1661f850d50f03f7cfc00ead02a956f0
Evan <dreamcoding@outlook.com>
2017-03-25 13:22:13 +0800

    update cluster-management.js

--- web/src/main/webapp/js/clusters-management.js
+++ web/src/main/webapp/js/clusters-management.js
@@ -2,16 +2,22 @@ $(function(){
 	$("#sidebar-menu-check-cluster").click(function(){
 		//alert("check cluster");	
 		resetMainContentHeader("Cluster Management","Check Cluster");
+		//empty old elements of main content
+		$("#center-main-content").empty();
 	});	
 	
 	$("#sidebar-menu-create-cluster").click(function(){
 		//alert("create cluster");
 		resetMainContentHeader("Cluster Management","Create Cluster");
+		//empty old elements of main content
+		$("#center-main-content").empty();
 	});	
 	
 	$("#sidebar-menu-monitor-cluster").click(function(){
 		//alert("monitor cluster");
 		resetMainContentHeader("Cluster Management","Monitor Cluster");
+		//empty old elements of main content
+		$("#center-main-content").empty();
 	});	
 });
 
