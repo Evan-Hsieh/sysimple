71533daca760cc64020e1b95c0ebdc8b9174996f
Evan <dreamcoding@outlook.com>
2017-04-10 22:29:13 +0800

    add host info form in web page of create cluster.

--- web/src/main/webapp/js/clusters-management.js
+++ web/src/main/webapp/js/clusters-management.js
@@ -4,13 +4,10 @@ $(function(){
 		resetMainContentHeader("Cluster Management","Check Cluster");
 		//empty old elements of main content
 		$("#center-main-content").empty();
-
 		syncAjaxInsertRow("#center-main-content","htmls/cluster-check-cluster-info-form.html");
 		syncAjaxInsertRow("#center-main-content","htmls/cluster-check-cluster-host-info-form.html");
-		syncAjaxInsertRow("#center-main-content","htmls/cluster-check-cluster-host-specific-detail-tabform.html");
-		
-		
-		
+		syncAjaxInsertRow("#center-main-content","htmls/cluster-check-cluster-host-specific-detail-tabform.html");		
+	
 	});	
 	
 	$("#sidebar-menu-create-cluster").click(function(){
@@ -19,6 +16,8 @@ $(function(){
 		//empty old elements of main content
 		$("#center-main-content").empty();
 		syncAjaxInsertRow("#center-main-content","htmls/cluster-create-cluster-set-cluster-info.html");
+		syncAjaxInsertRow("#center-main-content","htmls/cluster-create-cluster-set-host-info-form.html");
+		syncAjaxInsertRow("#create-cluster-host-info-form","htmls/cluster-create-cluster-set-host-info-row.html");
 		
 	});	
 	
